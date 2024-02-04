2bf31b4bc1d3601de412d7e4f60f48d5274c3e6d
Noor-Abdelmuneim <157888524+Noor-Abdelmuneim@users.noreply.github.com>
2024-02-04 13:30:49 +0300

    the task has been solved.

---
+++ index.js
@@ -0,0 +1,27 @@
+//Task 1:
+var name = 'noor'
+var age = 23;
+var isStudent = true
+console.log('my name is', name);
+console.log('my age is', age);
+console.log('student', isStudent);
+
+//Task 2:
+var variableToCheck = 'Noor Abdel Muneim'
+console.log('variable to check :', typeof variableToCheck);
+
+//Task 3:
+var greeting ='Hello'
+var name ='Noor'
+console.log(greeting + name);
+
+
+// Task 2:
+var a = 5
+var b = 3
+console.log('a grater than b', a > b);
+console.log('a less than b', a < b);
+console.log('a grater than or equal to b', a >= b);
+console.log('a less than or equal to b', a <= b);
+console.log('a equal to b', a == b);
+console.log('a not equal to b', a != b);
