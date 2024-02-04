d1815cdeadb5a388cdc570e21b209927fdbf5258
Noor-Abdelmuneim <157888524+Noor-Abdelmuneim@users.noreply.github.com>
2024-02-04 14:47:17 +0300

    h.w2 done

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-//Task 1:
-var name = 'noor'
-var age = 23;
-var isStudent = true
-console.log('my name is', name);
-console.log('my age is', age);
-console.log('student', isStudent);
-
-//Task 2:
-var variableToCheck = 'Noor Abdel Muneim'
-console.log('variable to check :', typeof variableToCheck);
-
-//Task 3:
-var greeting ='Hello'
-var name ='Noor'
-console.log(greeting + name);
-
-
 // Task 2:
 var a = 5
 var b = 3
